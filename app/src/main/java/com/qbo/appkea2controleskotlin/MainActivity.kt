07a1bf99f9e836f9274db55385125c374268c74e
LuisAngel <luisangel.salvat@gmail.com>
2020-11-06 17:24:24 -0500

    Initial commit

---
+++ app/src/main/java/com/qbo/appkea2controleskotlin/MainActivity.kt
@@ -0,0 +1,156 @@
+package com.qbo.appkea2controleskotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.CheckBox
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+    private val listapreferencias = ArrayList<String>()
+    private val listausuario = ArrayList<String>()
+    private var estadocivil = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        ArrayAdapter.createFromResource(this,
+            R.array.estado_civil_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter->
+            adapter.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item)
+            spestadocivil.adapter = adapter
+        }
+        spestadocivil.onItemSelectedListener = this
+
+        cbdeporte.setOnClickListener {
+            agregarPreferencia(it)
+        }
+        cbdibujo.setOnClickListener {
+            agregarPreferencia(it)
+        }
+        cbotros.setOnClickListener {
+            agregarPreferencia(it)
+        }
+
+        btnregistrar.setOnClickListener { vista ->
+            enviarMensaje(vista, "Hola Mundo")
+        }
+
+    }
+
+    private fun agregarPreferencia(vista: View) {
+        if(vista is CheckBox){
+            when(vista.id){
+                R.id.cbdeporte ->{
+                    if(vista.isChecked){
+                        listapreferencias.add(vista.text.toString())
+                    }else{
+                        listapreferencias.remove(vista.text.toString())
+                    }
+                }
+                R.id.cbdibujo ->{
+                    if(vista.isChecked){
+                        listapreferencias.add(vista.text.toString())
+                    }else{
+                        listapreferencias.remove(vista.text.toString())
+                    }
+                }
+                R.id.cbotros ->{
+                    if(vista.isChecked){
+                        listapreferencias.add(vista.text.toString())
+                    }else{
+                        listapreferencias.remove(vista.text.toString())
+                    }
+                }
+            }
+        }
+    }
+
+    fun validarFormulario(vista: View): Boolean{
+        var respuesta = false
+        if(!validarNombreApellido()){
+            enviarMensaje(vista, getString(R.string.errorNombreApellido))
+        } else if(!validarGenero()){
+            enviarMensaje(vista, getString(R.string.errorGenero))
+        }else if(!validarEstadoCivil()){
+            enviarMensaje(vista, getString(R.string.errorEstadoCivil))
+        } else if(!validarPreferencias()){
+            enviarMensaje(vista, getString(R.string.errorPreferencias))
+        }else{
+            respuesta = true
+        }
+        return respuesta
+    }
+
+    fun obtenerGeneroSeleccionado(): String{
+        var genero = ""
+        when(rggenero.checkedRadioButtonId){
+            R.id.rbmasculino -> {
+                genero = rbmasculino.text.toString()
+            }
+            R.id.rbfemenino ->{
+                genero = rbfemenino.text.toString()
+            }
+        }
+        return genero
+    }
+
+    fun validarPreferencias(): Boolean{
+        if(cbdeporte.isChecked || cbdibujo.isChecked || cbotros.isChecked){
+            return true
+        }
+        return false
+    }
+
+    fun validarEstadoCivil(): Boolean {
+        if(estadocivil == ""){
+            return false
+        }
+        return true
+    }
+
+    fun validarGenero(): Boolean{
+        var respuesta = true
+        if(rggenero.checkedRadioButtonId == -1){
+            respuesta = false
+        }
+        return respuesta
+    }
+
+    fun validarNombreApellido(): Boolean{
+        var respuesta = true
+        if(etnombre.text.toString().trim().isEmpty()){
+            etnombre.isFocusableInTouchMode = true
+            etnombre.requestFocus()
+            respuesta = false
+        } else if(etapellido.text.toString().trim().isEmpty()){
+            etapellido.isFocusableInTouchMode = true
+            etapellido.requestFocus()
+            respuesta = false
+        }
+        return respuesta
+    }
+
+    fun enviarMensaje(vista: View, mensaje: String){
+        Snackbar.make(vista, mensaje, Snackbar.LENGTH_LONG).show()
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        estadocivil = if(position > 0){
+            parent!!.getItemAtPosition(position).toString()
+        } else{
+            ""
+        }
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
+    }
+}
