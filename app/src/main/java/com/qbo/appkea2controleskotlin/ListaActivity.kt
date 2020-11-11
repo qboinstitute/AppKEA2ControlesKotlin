c6281337822230f3ae2ee1041988f2f30a962151
LuisAngel <luisangel.salvat@gmail.com>
2020-11-10 20:43:01 -0500

    Ejemplo terminado

--- app/src/main/java/com/qbo/appkea2controleskotlin/ListaActivity.kt
+++ app/src/main/java/com/qbo/appkea2controleskotlin/ListaActivity.kt
@@ -2,10 +2,31 @@ package com.qbo.appkea2controleskotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_lista.*
+import java.util.ArrayList
 
 class ListaActivity : AppCompatActivity() {
+
+    var listausuarios = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista)
+        //val parametro = intent.getDoubleExtra("paramdouble", 0.0)
+        //val parametro = intent.getStringExtra("paramdouble")
+        if(intent.hasExtra("listausuario")){
+            listausuarios = intent.getSerializableExtra("listausuario")
+                    as ArrayList<String>
+            val adapter = ArrayAdapter(
+                    this,
+                    android.R.layout.simple_list_item_1,
+                    listausuarios
+            )
+            lvusuario.adapter = adapter
+        }
+
+
+
     }
 }
--- app/src/main/java/com/qbo/appkea2controleskotlin/MainActivity.kt
+++ app/src/main/java/com/qbo/appkea2controleskotlin/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.qbo.appkea2controleskotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -40,9 +41,41 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         }
 
         btnregistrar.setOnClickListener { vista ->
-            enviarMensaje(vista, "Hola Mundo")
+            if(validarFormulario(vista)){
+                var infousuario = etnombre.text.toString()+ " - "+
+                        etapellido.text.toString() + " - "+
+                        obtenerGeneroSeleccionado() + " - " +
+                        obtenerPreferenciasSeleccionadas() + "-" +
+                        estadocivil+" - "+
+                        swemail.isChecked
+                listausuario.add(infousuario)
+                setearControles()
+                enviarMensaje(vista, "Usuario Registrado")
+            }
         }
-
+        btnlistar.setOnClickListener {
+            val intentLista = Intent(
+                    this,
+                    ListaActivity::class.java).apply {
+                putExtra("listausuario", listausuario)
+            }
+            startActivity(intentLista)
+            //Invocar a una actividad sin enviar parametros.
+            //startActivity(Intent(this, ListaActivity::class.java))
+        }
+    }
+    fun setearControles(){
+        listapreferencias.clear()
+        etnombre.setText("")
+        etapellido.setText("")
+        swemail.isChecked = false
+        cbotros.isChecked = false
+        cbdibujo.isChecked = false
+        cbdeporte.isChecked = false
+        rggenero.clearCheck()
+        spestadocivil.setSelection(0)
+        etnombre.isFocusableInTouchMode = true
+        etnombre.requestFocus()
     }
 
     private fun agregarPreferencia(vista: View) {
@@ -100,6 +133,14 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
             }
         }
         return genero
+    }
+
+    fun obtenerPreferenciasSeleccionadas(): String{
+        var preferencias = ""
+        for(pref in listapreferencias){
+            preferencias += "$pref - "
+        }
+        return preferencias
     }
 
     fun validarPreferencias(): Boolean{
